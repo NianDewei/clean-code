cfca41fdfd3ac29124d8ff29aa7df816602a9e9e
Rolando Oc <admi.jmb@gmail.com>
2023-04-09 09:33:35 -0500

    ⬆️ | applying DRY

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css"
 import typescriptLogo from "./typescript.svg"
 import viteLogo from "/vite.svg"
 import { setupCounter } from "./counter"
-import "./clean-code/04-homework"
+import "./clean-code/05-dry"
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
