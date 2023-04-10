fb444d442380dea36b9209884f8db84ab568cd17
Rolando Oc <admi.jmb@gmail.com>
2023-04-10 09:27:32 -0500

    ⬆️ | homework-classes > earring

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css"
 import typescriptLogo from "./typescript.svg"
 import viteLogo from "/vite.svg"
 import { setupCounter } from "./counter"
-import "./clean-code/06-classes-c"
+import "./clean-code/07-homework-classes"
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
